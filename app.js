9805402af3133d2c1f72e02eabe21a4ee6e6db26
Sokolov Aleksey <amsokolov79@gmail.com>
2020-08-18 14:15:49 +0700

    Значение keyCode выведенное в лог

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
  var main = function () {
  	"use strict";
+
  	$(".comment-input button").on("click", function (event) {
  		var $new_comment;
+
  		if ($(".comment-input input").val() !=="") {
  		 $new_comment = $("<p>").text($(".comment-input input").val());
  	    $(".comments").append($new_comment);
  	    $(".comment-input input").val("");
  		}
  	});
+ 	$(".comment-input input").on("keypress", function(event) {
+ 		console.log("Это значение keyCode "+ event.keyCode);
+ 	});
  };
+ 
+
  $(document).ready(main);
