e650c025332d21cecc185a2210d8f735dc5ae854
Sokolov Aleksey <amsokolov79@gmail.com>
2020-08-18 14:31:47 +0700

    Добавление через Enter

--- app.js
+++ app.js
@@ -10,8 +10,16 @@
  	    $(".comment-input input").val("");
  		}
  	});
- 	$(".comment-input input").on("keypress", function(event) {
- 		console.log("Это значение keyCode "+ event.keyCode);
+ 	$(".comment-input input").on("keypress", function (event) {
+ 		var $new_comment;
+ 		if (event.keyCode === 13) {
+ 			if ($(".comment-input input").val() !== "") {
+ 				var $new_comment = $ ("<p>").text($(".comment-input input").val());
+ 				$(".comments").append($new_comment);
+ 				$(".comment-input input").val("");
+ 			}
+ 		 
+ 		}
  	});
  };
  
