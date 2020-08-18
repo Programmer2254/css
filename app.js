28522088c6e535c8e440e59b98a2f048280dfab6
Sokolov Aleksey <amsokolov79@gmail.com>
2020-08-18 10:02:25 +0700

    Добави ввод комментария вариант 1

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
  var main = function () {
  	"use strict";
  	$(".comment-input button").on("click", function (event) {
- 	 $(".comments").append("<p>Это новый комментарий</p>");
+ 		var $new_comment = $("<p>"),
+ 		comment_text = $(".comment-input input").val();
+ 		$new_comment.text(comment_text);
+ 	    $(".comments").append($new_comment);
  	});
  };
  $(document).ready(main);
